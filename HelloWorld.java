f8d0bed0f58dc56aef2559401ee88ba4cc8be702
Srashti Srivastava <srivastavasrashti45@gmail.com>
2025-04-11 11:41:45 +0530

    changes

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+//package com.company;
+
+public class HelloWorld{   // HelloWorld is the name of the class, which is same as file name.
+    public static void main(String[] args){    
+        //main is function, void means it is not returning any value,
+        // public is access modifier, 
+        //static is used to use the function independently without object and still residr in the class.
+        System.out.println("Hello World");
+    }
+}
+
+
+// Naming Convention
+// 1- For classes we use PascalConvention.(Ex-SrashtiSrivastava)
+// 2- For Functions we use camelCaseConvention.(Ex-nameSrashtiSrivastava)
+
+// A package is used to group similar type of code, and it is mostly associated with the company name.
