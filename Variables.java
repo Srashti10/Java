72ad3a601dc6f71d8ba6d8b0716affadf8172b90
Srashti Srivastava <srivastavasrashti45@gmail.com>
2025-04-12 14:45:42 +0530

    changes

--- Variables.java
+++ Variables.java
@@ -1,12 +1,16 @@
 public class Variables{
     public static void main(){
-        int a = 2;
-        int b = 3;
-        int c = 4;
-        // char aa = "Srashti";
-        // char bb = "Srivastava";
-        System.out.println(a+b+c);
-        // System.out.println(aa+bb);
+        int a = 20;
+        int b = 30;
+        int c = 40;
+        System.out.println(a+b+c); 
+        System.out.print(a);  
+        System.out.print(b);  
+        System.out.print(c);  
 
+        //println will print a new line after every staement.
+        //print will print the statements in the same line.
     }
 }
+// Data types in Java: 1- Primitive  2- Non-Primitive
+// 1-Primitive: byte, short, int, float, long, double, char, boolean
