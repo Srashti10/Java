e08e0d6a494592f4d6d479e1c182a84da7c891a5
Srashti Srivastava <srivastavasrashti45@gmail.com>
2025-04-12 11:14:59 +0530

    changes

--- Variables.java
+++ Variables.java
@@ -3,6 +3,10 @@ public class Variables{
         int a = 2;
         int b = 3;
         int c = 4;
+        // char aa = "Srashti";
+        // char bb = "Srivastava";
         System.out.println(a+b+c);
+        // System.out.println(aa+bb);
+
     }
 }
