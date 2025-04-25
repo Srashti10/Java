ed2eb3ca951daae16afefdd43ff825d67fe8cdc8
Srashti Srivastava <srivastavasrashti45@gmail.com>
2025-04-25 14:11:07 +0530

    changes

--- TakingInput.java
+++ TakingInput.java
@@ -1,34 +1,20 @@
-// import java.util.Scanner; // This will import the Scanner class in Java.
-// public class TakingInput {
-//     public static void main(String[] args) {
-//         System.out.println("Taking input from the user.");
-//         Scanner sc = new Scanner(System.in); // Create a Scanner object (named sc). System.in to read input from the keyboard.
-//         // Scanner class has a lot of methods to read the data.
-//         // 1. nextInt() - to read an integer value. 
-//         // 2. nextFloat() - to read a float value.
-//         System.out.println("Enter number 1: ");
-//         int a = sc.nextInt(); // Read an integer value from the user and store it in a. (sc) is the object of Scanner class.
-//         System.out.println("Enter number 2: ");
-//         int b = sc.nextInt(); // Read an integer value from the user and store it in b.
-//         int sum = a+b;
-//         System.out.println("Sum of " + a + " and " + b + " is: " + sum); // Print the sum of a and b.
-//         // sc.hasNextInt() method is used to check if the next input is an integer.
+import java.util.Scanner; // This will import the Scanner class in Java.
+public class TakingInput {
+    public static void main(String[] args) {
+        System.out.println("Taking input from the user.");
+        Scanner sc = new Scanner(System.in); // Create a Scanner object (named sc). System.in to read input from the keyboard.
+        // Scanner class has a lot of methods to read the data.
+        // 1. nextInt() - to read an integer value. 
+        // 2. nextFloat() - to read a float value.
+        System.out.println("Enter number 1: ");
+        int a = sc.nextInt(); // Read an integer value from the user and store it in a. (sc) is the object of Scanner class.
+        System.out.println("Enter number 2: ");
+        int b = sc.nextInt(); // Read an integer value from the user and store it in b.
+        int sum = a+b;
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum); // Print the sum of a and b.
+        // sc.hasNextInt() method is used to check if the next input is an integer.
 
-//     }
-// }
-
-
-import java.util.Scanner;
-public class TakingInput{
-    public static void main(String[] args){
-        System.out.println("Hello World.");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first number:");
-        int num1 = sc.nextInt();
-        System.out.println("Enter second number:");
-        int num2 = sc.nextInt();
-        int sum = num1 + num2;
-        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
     }
 }
+
 
